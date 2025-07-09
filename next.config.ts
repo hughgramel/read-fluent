0099b0922564bf1fa5202fa2cadad4f61d2c1a9e
Hugh Gramelspacher <hughgramelspacher@gmail.com>
2025-07-09 16:03:28 -0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,10 @@
 import type { NextConfig } from "next";
-import withPWA from 'next-pwa';
 
-const nextConfig: NextConfig = withPWA({
+const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
-});
+};
 
 export default nextConfig;
